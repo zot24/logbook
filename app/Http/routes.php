7233325139b448ba8616c723b6af0dbc62779744
Israel Sotomayor <sotoisra24@gmail.com>
2015-02-01 10:33:20 +0000

    new: add routes needed to support CRUD on records

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,28 @@ Route::get('/', ['uses' => 'Auth\AuthController@getLogin', 'as' => 'home']);
 Route::get('login',   ['uses' => 'Auth\AuthController@getLogin',  'as' => 'session.create']);
 Route::post('login',  ['uses' => 'Auth\AuthController@postLogin', 'as' => 'session.store']);
 Route::get('logout',  ['uses' => 'Auth\AuthController@getLogout', 'as' => 'session.destroy']);
+
+/**
+ * Registration
+ *
+ * Route to the register
+ */
+Route::get('register',  ['uses' => 'Auth\AuthController@getRegister',  'as' => 'register.create']);
+Route::post('register', ['uses' => 'Auth\AuthController@postRegister', 'as' => 'register.store']);
+
+/**
+ * Records
+ *
+ * Route to index, create, store, show, edit, update, delete and destroy records
+ */
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('records',             ['uses' => 'RecordsController@index',   'as' => 'records.index']);
+    Route::get('records/create',      ['uses' => 'RecordsController@create',  'as' => 'records.create']);
+    Route::post('records',            ['uses' => 'RecordsController@store',   'as' => 'records.store']);
+    Route::get('records/{id}',        ['uses' => 'RecordsController@show',    'as' => 'records.show']);
+    Route::get('records/{id}/edit',   ['uses' => 'RecordsController@edit',    'as' => 'records.edit']);
+    Route::put('records/{id}',        ['uses' => 'RecordsController@update',  'as' => 'records.update']);
+    Route::patch('records/{id}',      ['uses' => 'RecordsController@update',  'as' => 'records.update']);
+    Route::get('records/{id}/delete', ['uses' => 'RecordsController@delete',  'as' => 'records.delete']);
+    Route::delete('records/{id}',     ['uses' => 'RecordsController@destroy', 'as' => 'records.destroy']);
+});
