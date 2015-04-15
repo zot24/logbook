a8e7dd38107312578d6348011ba9edf4b2e77017
Israel Sotomayor <sotoisra24@gmail.com>
2015-04-15 23:05:12 +0100

    chg: code styling !minor

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  @include('includes.head', ['css' => 'css/frontend.css'])
+    @include('includes.head', ['css' => 'css/frontend.css'])
 </head>
 <body>
-  <div class="container">
+<div class="container">
     @yield('content')
-  </div>
-  @include('includes.footer', ['js' => 'js/frontend.js'])
+</div>
+@include('includes.footer', ['js' => 'js/frontend.js'])
 </body>
 </html>
